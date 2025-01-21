d6b4f96ef7c489492c8aa6b600d734be9743c3cc
ungaul <debaux.julien@gmail.com>
2025-01-21 14:04:51 +0900

    OS adaptive

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -1,9 +1,28 @@
+
 document.addEventListener('DOMContentLoaded', async () => {
     try {
         const response = await fetch('https://api.github.com/repos/justin025/onthespot/releases/latest');
         const release = await response.json();
-        const asset = release.assets.find(a => a.name.endsWith('.exe') && (a.name.includes('x86') || a.name.includes('64')));
-        if (asset) document.getElementById('download').href = asset.browser_download_url;
+
+        const platform = navigator.platform.toLowerCase();
+        let fileExtension = '';
+
+        if (platform.includes('win')) {
+            fileExtension = '.exe';
+        } else if (platform.includes('mac')) {
+            fileExtension = '.dmg';
+        } else if (platform.includes('linux')) {
+            fileExtension = '.AppImage';
+        } else {
+            throw new Error('Unsupported platform');
+        }
+
+        const asset = release.assets.find(a => a.name.endsWith(fileExtension));
+        if (asset) {
+            document.getElementById('download').href = asset.browser_download_url;
+        } else {
+            console.error(`No file found for platform: ${platform}`);
+        }
     } catch (error) {
         console.error('Error fetching the latest release:', error);
     }
